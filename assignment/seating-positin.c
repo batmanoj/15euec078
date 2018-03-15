16009d62378fbfc30ef3f25e8faf562f8bf6d82d
batmanoj <34646136+batmanoj@users.noreply.github.com>
2018-03-15 21:35:58 +0530

    Update seating-positin.c

--- assignment/seating-positin.c
+++ assignment/seating-positin.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
+int main() 
+{
+    int n,i,x;
+    scanf("%d",&n);
+    if(n>6)
+    {
+        do
+        {
+            x=n%6;
+        }while(x>6);
+        i=x;
+    }else
+    {
+        i=n;
+    }
+    
+    if(i==1||i==6||i==0)
+    {
+        printf("WS");
+    }else if(i==2||i==5)
+    {
+        printf("MS");
+    }else if(i==3||i==4)
+    {
+        printf("AS");
+    }
+    
+   
+}
 
