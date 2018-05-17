2c541b550d48ec214bc33a6250c082471b1afd66
batmanoj <34646136+batmanoj@users.noreply.github.com>
2018-05-17 21:35:17 +0530

    Create h-1-4.c

---
+++ assignment/h-1-4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+   int a[1000],i,j,N,c;
+   scanf("%d",&N);
+   for(i=0;i<N;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for (i=0;a[i]!='\0';i++) 
+    {
+        c=0;
+        for (j=0;a[j]!='\0';j++)
+        {
+            
+            if (a[j]==a[i]) 
+            {
+                c++;
+            }
+        }
+        if(c==1)
+        {
+            printf("%d ",a[i]);
+        }
+    }
+}
