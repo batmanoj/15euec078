939a2f10ac029a093a5087a0ff03bd6866913590
batmanoj <34646136+batmanoj@users.noreply.github.com>
2018-05-22 22:32:52 +0530

    Create h-3-6.c

---
+++ assignment/h-3-6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+   int a[10000],b[100000],n,i,j=0;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(i=n-1;i>=0;i--)
+   {
+       b[j]=a[i];
+       j++;
+   }
+   for(i=0;i<n;i++)
+   {
+       printf("%d",b[i]);
+   }
+   
+}
