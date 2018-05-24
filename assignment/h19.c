89047e1ad6a2798074ce4437c0897dc37b6c88af
batmanoj <34646136+batmanoj@users.noreply.github.com>
2018-05-24 23:14:10 +0530

    Create h19.c

---
+++ assignment/h19.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main() 
+{
+   int a[100000][100000],i,N,K;
+   scanf("%d%d",&N,&K);
+   for(i=0;i<N;i++)
+   {
+       for(j=0;j<K;j++)
+       {
+            scanf("%d",&a[i][j]);
+       }
+   }
+   for(i=0;i<n;i++)
+   {
+       b[i]=a[1][i];
+   }
+   for(i=0;i<N;i++)
+   {
+       for(j=0;j<K;j++)
+       {
+           
+       } 
+   }
+   
+}
